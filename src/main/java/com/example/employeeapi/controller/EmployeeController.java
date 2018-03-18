32129723fa8001bda4ea4b38a7814416ded3cb4e
Hong Ma <hma@thoughtworks.com>
2018-03-18 16:40:58 +0800

    add employee.jsp

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,26 +1,26 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
-import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping("/employees")
-    ResponseEntity getEmployees() {
-        return new ResponseEntity<>(employeeRepository.getAllEmployees(), HttpStatus.OK);
+    @RequestMapping(value = "/employees",method = RequestMethod.GET)
+    String getEmployees(Model model) {
+        model.addAttribute("message", "mmmmmm");
+
+//        return new ResponseEntity<>(employeeRepository.getAllEmployees(), HttpStatus.OK);
+        return "employees";
     }
 
     @GetMapping("/employees/{id}")
---
+++ src/main/webapp/WEB-INF/jsp/employees.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html;charset=UTF-8"  language="java" %>>
+<html>
+<head>
+    <title>huhu</title>
+</head>
+<body>
+
+<div>Hello ${message}</div>
+
+</body>
+</html>
